303379d5a3b5507fa5db88469daf0d39956461a7
Tom Victor <vjtomvictor@icloud.com>
2017-09-29 16:25:36 +0530

    update

--- main.py
+++ main.py
@@ -4,27 +4,36 @@ import json
 print("Fake person starting...")
 data = ""
 
-with urllib.request.urlopen('https://uinames.com/api/?ext&gender=female&region=England') as f:
-	print("checking API...\n")
-	# print(f.read(300))
-	# saving response as byte
-	data =f.read()
+def faker():
+	
+	with urllib.request.urlopen('https://uinames.com/api/?ext&gender=female&region=England') as f:
+		print("checking API...\n")
+		# print(f.read(300))
+		# saving response as byte
+		data =f.read()
+		# decoding data in utf-8 format
+		response = data.decode('utf-8')
 
-# decoding data in utf-8 format
-response = data.decode('utf-8')
+		data = json.loads(response)
+		print(data)
+		print("\n")
+		print(data["name"])
+		print(data["surname"])
+		print(data["gender"])
+		print(data["age"])
+		print(data["birthday"]["dmy"])
+		print(data["phone"])
+		print(data["email"])
+		print(data["photo"])
+		print(data["region"])
 
-data = json.loads(response)
-print(data)
-print("\n")
-print(data["name"])
-print(data["surname"])
-print(data["gender"])
-print(data["age"])
-print(data["birthday"]["dmy"])
-print(data["phone"])
-print(data["email"])
-print(data["photo"])
-print(data["region"])
+		pic_name = "img/" + data["name"] + ".jpg"
+
+		urllib.request.urlretrieve(data["photo"], pic_name)
 
 
+
+for item in range(1,100):
+	faker()
+	print(item)
 
